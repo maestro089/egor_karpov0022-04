9d89b133e3e4d3470b56d631ad3954827df2b6f4
maestro089 <65550927+maestro089@users.noreply.github.com>
2021-03-12 15:15:41 +0300

    Add files via upload

---
+++ 6.1.py
@@ -0,0 +1,40 @@
+str = []
+str_3 = []
+
+def fun():
+    str = []
+    while(True):
+        check = input()
+        if check == "quit":
+            break
+        else:
+            str.append(check)
+    return str
+
+def fun2(str):
+    str_3 = []
+    for i in range(len(str)):
+        check = input()
+        str_3.append(check)
+    return str_3
+
+def fun3(str_3,str):
+    s = {str_3[i]:str[i] for i in range(len(str))}
+    return s
+    
+    
+
+print("Для выхода из цикла введите - quit")
+str = fun()
+
+str_2 = set(str)
+print("список -",str)
+print("множество - ",str_2)
+print("количество символов в cgbcrt =",len(str))
+print("заполните следующий список из",len(str),"символов")
+
+str_3 = fun2(str)
+
+print(str_3)
+s = fun3(str_3,str)
+print(s)
