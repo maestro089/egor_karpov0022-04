f1e51e571bf3cc5ce5ef630c1a644e07a46aa6b8
maestro089 <65550927+maestro089@users.noreply.github.com>
2021-03-31 15:08:11 +0300

    Update math_fun.py

--- 8/math_fun.py
+++ 8/math_fun.py
@@ -1,60 +1,60 @@
 import math
 import random
 
+class m():
+    def factorial(n):
+        if n == 0:
+            return 1
+        return n*factorial(n-1)
 
-def factorial(n):
-    if n == 0:
-        return 1
-    return n*factorial(n-1)
+    def sum():
+        print("a =")
+        a = float(input())
+        print("b = ")
+        b = float(input())
+        print(a+b)
 
-def sum():
-    print("a =")
-    a = float(input())
-    print("b = ")
-    b = float(input())
-    print(a+b)
-    
-def de():
-    print("a =")
-    a = float(input())
-    print("b = ")
-    b = float(input())
-    print(a/b)
-    
-def node():
-    print("a =")
-    a = float(input())
-    print("b = ")
-    b = float(input())
-    print(a*b)
-        
-def nosum():
-    print("a =")
-    a = float(input())
-    print("b = ")
-    b = float(input())
-    print(a-b)
-        
-def s():
-    print("a =")
-    a = float(input())
-    print("b = ")
-    b = float(input())
-    print(a ** b)
-        
-def mod():
-    print("a =")
-    a = float(input())
-    print(math.fabs(a))
-        
-def acos():
-    print("a =")
-    a = float(input())
-    print(math.acos(a))
-    
-def r():
-    print("рандомное число в диапазона от = ")
-    a = int(input())
-    print("до =")
-    b = int(input())
+    def de():
+        print("a =")
+        a = float(input())
+        print("b = ")
+        b = float(input())
+        print(a/b)
+
+    def node():
+        print("a =")
+        a = float(input())
+        print("b = ")
+        b = float(input())
+        print(a*b)
+
+    def nosum():
+        print("a =")
+        a = float(input())
+        print("b = ")
+        b = float(input())
+        print(a-b)
+
+    def s():
+        print("a =")
+        a = float(input())
+        print("b = ")
+        b = float(input())
+        print(a ** b)
+
+    def mod():
+        print("a =")
+        a = float(input())
+        print(math.fabs(a))
+
+    def acos():
+        print("a =")
+        a = float(input())
+        print(math.acos(a))
+
+    def r():
+        print("рандомное число в диапазона от = ")
+        a = int(input())
+        print("до =")
+        b = int(input())
     print(random.randint(a,b))
