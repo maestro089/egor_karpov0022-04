ea1d7b1393a7c9cbda969a9aaa67d03e7b3ca896
maestro089 <65550927+maestro089@users.noreply.github.com>
2021-04-01 17:54:49 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,56 +1,61 @@
-
-
-def fun(name):
-    str_1 = []
-    str_2 = []
+def writing_file(name):
+    
+    mass_1 = []
+    
     r = 0
-    file = open(name + ".txt","r")#
+    
+    file = open(name + ".txt","r")
+    
     #заполняю массив строками
     for line in file:
-        str_1.append(line)
+        mass_1.append(line)
     file.close
     file = open(name + ".txt","w")
-    for key,val in str.items():
+    
+    for key,val in dictionary.items():
         file.write('{}:{}\n'.format(key,val))
     file.close
     file = open(name + ".txt","a")
 
-    for i in range(len(str_1)):
-        file.write(str_1[i])
+    for i in range(len(mass_1)):
+        file.write(mass_1[i])
     file.close
 
-    file = open(name + ".txt","r")
-    file.close
+
     
-str = []
-str_3 = []
+mass = []
+mass_3 = []
+
 print("Для выхода из цикла введите - quit")
+
 while(True):
     check = input()
     if check == "quit":
         break
     else:
-        str.append(check)
-str_2 = set(str)
-print("список -",str)
-print("множество - ",str_2)
-print("количество символов в cgbcrt =",len(str))
-print("заполните следующий список из",len(str),"символов")
-for i in range(len(str)):
+        mass.append(check)
+        
+set_2 = set(mass)
+print("список -",mass)
+print("множество - ",set_2)
+print("количество символов в cgbcrt =",len(mass))
+print("заполните следующий список из",len(mass),"символов")
+
+for i in range(len(mass)):
     check = input()
-    str_3.append(check)
-print(str_3)
-s = {str_3[i]:str[i] for i in range(len(str))}
+    mass_3.append(check)
+    
+print(mass_3)
+s = {mass_3[i]:mass[i] for i in range(len(mass))}
 
 print("Введите название файла")
 name = input()
 
-str = s
+dictionary = s
 
 try:
-    fun(name)
+    writing_file(name)
 except IOError as e:
     file = open(name + ".txt","a")
-    fun(name)
-
+    writing_file(name)
 
